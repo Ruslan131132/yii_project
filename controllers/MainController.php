5f206246379fe8a8bfba1bc3ad410568031dcfae
Ruslan131132 <ruskhas131@mail.ru>
2022-02-22 02:29:18 +0300

    Добавлены методы для авторизации

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -3,19 +3,47 @@
 namespace app\controllers;
 
 use app\models\Post;
-use yii\base\Controller;
+use Yii;
+use app\models\LoginForm;
+use yii\base\BaseObject;
+use yii\web\Controller;
 
 class MainController extends Controller {
 
     public function actionIndex() {
-        $posts = Post::find()->all();
+        $posts = Post::find()->limit(12)->all();
+        $last_posts = Post::find()->orderBy('created_at DESC')->limit(3)->all();
         return $this->render('index', [
-            'posts' => $posts
+            'posts' => $posts,
+            'last_posts' => $last_posts
         ]);
     }
 
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
     public function actionError() {
-//        $posts = Post::find()->all();
         return $this->render('error');
     }
 }
